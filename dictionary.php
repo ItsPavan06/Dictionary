3f8a7583162bde88f8742e23196ce1e5cd5f4c8e
Pavan Bhat <pavankabbinagadde06@gmail.com>
2026-08-11 19:05:51 +0530

    UI for entering the word to get meaning

--- dictionary.php
+++ dictionary.php
@@ -1,3 +1,16 @@
-<?php
-echo "Hello welcome to PHP server side language!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Searching</title>
+</head>
+<body>
+    <h2>DICTIONARY</h2>
+
+    <form method="POST" action="">
+        <label for="word">Enter a word</label>
+        <input type="text" name="txt" id="wrd" required>
+        
+        <input type="submit" name="sbtn" value="search">
+    </form>
+</body>
+</html>
